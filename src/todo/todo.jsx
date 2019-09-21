387d4a598660758c07fd6dab3e8e012940de6ace
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-09-21 16:10:14 -0300

    Melhorias de CSS e limpar formulario

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -19,6 +19,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleClear = this.handleClear.bind(this);
 
         this.refresh();
     }
@@ -57,6 +58,10 @@ export default class Todo extends Component {
             .then(response => this.refresh(this.state.description));
     }
 
+    handleClear() {
+        this.refresh();
+    }
+
     render() {
         return (
             <div>
@@ -64,7 +69,8 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description} 
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}
-                    handleSearch={this.handleSearch}/>
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}/>
                 <TodoList list={this.state.list} 
                     handleRemove={this.handleRemove}
                     handleMarkAsDone={this.handleMarkAsDone}
