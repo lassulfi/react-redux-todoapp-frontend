10ba045b14ac395b4ec871475df8efae60f47915
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-11-07 21:56:34 -0300

    Melhorando a action da pesquisa

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,81 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PageHeader from '../template/page-header';
 import TodoForm from './todo-form'
 import TodoList from './todo-list';
 
-const URL = 'http://localhost:3003/api/todos';
+// const URL = 'http://localhost:3003/api/todos';
 
-export default class Todo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            description: '',
-            list: []
-        }
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleRemove = this.handleRemove.bind(this);
-        this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
-        this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-        this.handleClear = this.handleClear.bind(this);
-
-        this.refresh();
-    }
-
-    refresh(description = '') {
-        const search = description ? `&description__regex=/${description}/` : '';
-        axios.get(`${URL}?sort=-createdAt${search}`).then(response => 
-            this.setState({...this.state, description, list: response.data}));
-    }
-
-    handleSearch() {
-        this.refresh(this.state.description);
-    }
-
-    handleAdd() {
-        const description = this.state.description;
-        axios.post(URL, {description}).then(response => this.refresh());
-    }
-
-    handleChange(e) {
-        this.setState({...this.state, description: e.target.value});
-    }
-
-    handleRemove(todo) {
-        axios.delete(`${URL}/${todo._id}`)
-            .then(response => this.refresh(this.state.description));
-    }
-
-    handleMarkAsDone(todo) {
-        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
-            .then(response => this.refresh(this.state.description));
-    }
-
-    handleMarkAsPending(todo) {
-        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
-            .then(response => this.refresh(this.state.description));
-    }
-
-    handleClear() {
-        this.refresh();
-    }
-
-    render() {
-        return (
-            <div>
-                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm description={this.state.description} 
-                    handleAdd={this.handleAdd}
-                    handleChange={this.handleChange}
-                    handleSearch={this.handleSearch}
-                    handleClear={this.handleClear}/>
-                <TodoList list={this.state.list} 
-                    handleRemove={this.handleRemove}
-                    handleMarkAsDone={this.handleMarkAsDone}
-                    handleMarkAsPending={this.handleMarkAsPending} />    
-            </div>
-        )
-    }
-}
+export default props => (
+    <div>
+        <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+        <TodoForm />
+        <TodoList />    
+    </div>
+)
