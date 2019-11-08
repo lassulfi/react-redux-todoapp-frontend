cb7d3e4f9e4912312251c962b8d412fc9e488e8b
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-11-07 21:24:29 -0300

    Limpar formulario

--- src/todo/todo-form.jsx
+++ src/todo/todo-form.jsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import Grid from '../template/grid';
 import IconButton from '../template/icon-button'
 
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -18,11 +18,11 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        const { add, search, description } = this.props
+        const { add, search, clear, description } = this.props
         if (e.key === 'Enter') {
             e.shiftKey ? search() : add(description);
         } else if (e.key === 'Escape') {
-            props.handleClear();
+            clear();
         }
     }
 
@@ -40,7 +40,7 @@ class TodoForm extends Component {
                 <Grid cols='12 3 2'>
                     <IconButton style='primary' icon='plus' onClick={() => add(description)}></IconButton>
                     <IconButton style='info' icon='search' onClick={() => search()}></IconButton>
-                    <IconButton style='default' icon='close' onClick={this.props.handleClear}></IconButton>
+                    <IconButton style='default' icon='close' onClick={this.props.clear}></IconButton>
                 </Grid>
             </div>
         )
@@ -49,6 +49,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({ description: state.todo.description })
 
-const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -27,7 +27,7 @@ export const add = (description) => {
 export const add = (description) => {
     return dispatch => {
         axios.post(URL, { description })
-            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(resp => dispatch(clear()))
             .then(resp => dispatch(search()))
     }
 }
@@ -52,4 +52,8 @@ export const remove = (todo) => {
         axios.delete(`${URL}/${todo._id}`)
             .then(resp => dispatch(search()))
     }
+}
+
+export const clear = () => {
+    return { type: 'TODO_CLEAR' }
 }
